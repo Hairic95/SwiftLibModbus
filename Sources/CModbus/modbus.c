1b1465d3830816c32528a6f81f1697787ca4b46d
Enrico Rossetti <hairic95@gmail.com>
2022-09-09 17:52:16 +0200

    Update modbus.c

--- Sources/CModbus/modbus.c
+++ Sources/CModbus/modbus.c
@@ -1636,7 +1636,7 @@ void _modbus_init_common(modbus_t *ctx)
     ctx->debug = FALSE;
     ctx->error_recovery = MODBUS_ERROR_RECOVERY_NONE;
 
-    ctx->response_timeout.tv_sec = 0;
+    ctx->response_timeout.tv_sec = _RESPONSE_SECONDS_TIMEOUT;
     ctx->response_timeout.tv_usec = _RESPONSE_TIMEOUT;
 
     ctx->byte_timeout.tv_sec = 0;
